20b1a73153a59a41c408b518d40876e9dc436a83
Christopher Gauthier <54082623+chrisgaut@users.noreply.github.com>
2020-06-26 22:50:00 -0400

    Update sophomore_slump.R

--- Sophomore-Slump/sophomore_slump.R
+++ Sophomore-Slump/sophomore_slump.R
@@ -1,19 +1,16 @@
-## ----------------------------------------------------------------------------
-## Personal Work - Sophomore Slump?
-## Twitter Chart turned Article
+## Investigating Sophomore Slump
+## Blog Posts
 ## Christopher Gauthier - 02/06/2020
-## ----------------------------------------------------------------------------
-# Load libraries
+
+# Load libraries #
 library(tidyverse)
 library(readxl)
 
-# PNG 900 x 550
-
-# Read in hitter and pitcher data
+# Read in hitter and pitcher data #
 hitters <- read_excel("/Users/chrisgauthier/ChrisDocs/Data Work/New Data Projects/Proj2_SophomoreSlump/Rookie2Sophomore.xlsx", sheet = 1)
 pitchers <- read_excel("/Users/chrisgauthier/ChrisDocs/Data Work/New Data Projects/Proj2_SophomoreSlump/Rookie2Sophomore.xlsx", sheet = 2)
 
-# Create change line plots
+# Create line plots depicting change in statistics #
 OPSplot <- ggplot(hitters, aes(x = Season, y = OPSplus, group = Player, color = Player)) +
   geom_point(stat = "summary", fun.y = sum) +
   stat_summary(fun.y = sum, geom = "line") + 
@@ -26,14 +23,14 @@ ERAplot <- ggplot(pitchers, aes(x = Season, y = ERAplus, group = Player, color =
   ggtitle("Change in ERA+ from Rookie to Sophomore Season, ROTY Winners Since 2010") +
   ylab("ERA+")
 
-# Create new metrics
+# Create new metric: bWARper100 #
 hitters <- hitters %>%
   mutate(bWARper100 = (bWAR * 100)/PA)
 
 pitchers <- pitchers %>%
   mutate(bWARper100 = (bWAR * 100)/BF)
 
-# Create change line plots (WAR)
+# Create line plots depicting change in WAR #
 WARperPAplot <- ggplot(hitters, aes(x = Season, y = bWARper100, group = Player, color = Player)) +
   geom_point(stat = "summary", fun.y = sum) +
   stat_summary(fun.y = sum, geom = "line") + 
@@ -46,13 +43,14 @@ WARperBFplot <- ggplot(pitchers, aes(x = Season, y = bWARper100, group = Player,
   ggtitle("Change in WAR per 100 BF from Rookie to Sophomore Season, ROTY Winners Since 2010") +
   ylab("WAR/100 BF")
 
-# Average Hitter Triple Slash
+# Average Hitter Triple Slash #
 averageTripleSlash <- hitters %>%
   group_by(Season) %>%
   summarize(AVG = sum(H)/sum(AB),
             OBP = (sum(H) + sum(BB) + sum(HBP))/(sum(AB) + sum(BB) + sum(HBP) + sum(SF)),
             SLG = sum(TB)/sum(AB))
 
+# Average Pitcher Statistics #
 averagePitcherStats <- pitchers %>%
   group_by(Season) %>%
   summarize(ERA = sum(ER)/sum(IP) * 9,
